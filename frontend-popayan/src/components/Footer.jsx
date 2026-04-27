8fbe0ea9c3094d3f2feaba9fa02147f8116f4c64
julian salinas <julioss838@gmail.com>
2026-04-27 18:31:04 -0500

    fix: correccion de case-sensitive en import de logo

--- frontend-popayan/src/components/Footer.jsx
+++ frontend-popayan/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/Logo.png';
+import logo from '../assets/logo.png';
 import { Facebook, Instagram, Twitter, Mail, MapPin, Sparkles } from 'lucide-react';
 
 const Footer = () => {
