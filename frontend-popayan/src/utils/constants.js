b071ab91118d743fb75b27f65f01b03781dd7b90
julian salinas <julioss838@gmail.com>
2026-05-03 13:21:44 -0500

    fix: Inyección dinámica de VITE_API_URL en Auth

--- frontend-popayan/src/utils/constants.js
+++ frontend-popayan/src/utils/constants.js
@@ -1,5 +1,5 @@
 // src/utils/constants.js
 export const ASSETS = {
-  // 🔥 URL REAL DE CLOUDINARY (Auditada desde tu Media Library)
-  LOGO_PRINCIPAL: 'https://res.cloudinary.com/ds7mev8mi/image/upload/v1714755589/popayan/logo/logo_v2_f8p6r9.png',
+  // 🔥 URL ABSOLUTA CORREGIDA (.png)
+  LOGO_PRINCIPAL: 'https://res.cloudinary.com/dnmyy7v0g/image/upload/v1777820386/logo_home_d3cddh.png',
 };
